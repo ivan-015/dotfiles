65f9d00e7f0b072c38321a0a01ae9e30b786c625
Ivan Vigliante <ivan.avr@protonmail.com>
2024-03-29 23:49:52 -0700

    Add bind to find references with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>fr', function()
+    builtin.lsp_references()
+end)
