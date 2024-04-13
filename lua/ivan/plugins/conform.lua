f94406f175d58da15f03bbe9f25c77b1e70de31c
Ivan Vigliante <ivan.avr@protonmail.com>
2024-04-12 22:43:02 -0700

    Replace prettier with conform

---
+++ lua/ivan/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                -- Use a sub-list to run only the first available formatter
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true
+            }
+        })
+        vim.api.nvim_create_user_command("Format", function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                range = {
+                    start = { args.line1, 0 },
+                    ["end"] = { args.line2, end_line:len() },
+                }
+            end
+            require("conform").format({ async = true, lsp_fallback = true, range = range })
+        end, { range = true })
+    end
+}
--- lua/ivan/plugins/prettier.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'prettier.nvim',
-    dependencies = {
-        'jose-elias-alvarez/null-ls.nvim',
-        'MunifTanjim/prettier.nvim'
-    }
-}
