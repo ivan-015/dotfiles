267a924e618ac0e162504ddc0548256e53404460
Ivan Vigliante <ivan.avr@protonmail.com>
2024-04-16 17:09:47 -0700

    Add lualine plugin

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,10 +3,11 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({ buffer = bufnr })
 
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+  vim.keymap.set("n", "<leader>c", vim.lsp.buf.code_action, bufopts)
 end)
 
 require('mason').setup({})
@@ -15,5 +16,3 @@ require('mason-lspconfig').setup({
     lsp_zero.default_setup,
   },
 })
-
-
