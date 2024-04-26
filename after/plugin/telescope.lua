a580e2bee709dbe7501acbe95d25581c5d07e538
Ivan Vigliante <ivan.avr@protonmail.com>
2024-04-25 17:37:46 -0700

    Add oldfiles mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
 
 vim.keymap.set('n', '<leader>fr', function()
   builtin.lsp_references()
