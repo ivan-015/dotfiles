091c2cd13cef4ed10683b6b3b3d76b04f6ea42d7
Ivan Vigliante <ivan.avr@protonmail.com>
2024-04-26 21:06:37 -0700

    Add mappings to lsp autocomplete

--- lua/ivan/plugins/lsp-zero.lua
+++ lua/ivan/plugins/lsp-zero.lua
@@ -1,17 +1,40 @@
 return {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            --- Uncomment the two plugins below if you want to manage the language servers from neovim
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
-        }
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'L3MON4D3/LuaSnip' },
+  },
+  config = function()
+    local cmp = require('cmp');
+    cmp.setup({
+      mapping = {
+        ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+        ['<C-p>'] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = 'insert' })
+          else
+            cmp.complete()
+          end
+        end),
+        ['<C-n>'] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = 'insert' })
+          else
+            cmp.complete()
+          end
+        end),
+      }
+    })
+  end
 }
-
