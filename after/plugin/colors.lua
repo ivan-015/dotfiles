ccfc0b3f0ea14cf501a9029ce2261557bdf3692b
Ivan Vigliante <ivan.avr@protonmail.com>
2024-04-27 11:56:12 -0700

    Edit line colors to stand out more

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,13 @@
 function ApplyColorScheme(color)
-	color = color or 'catppuccin'
-	vim.cmd.colorscheme(color)
+  color = color or 'catppuccin'
+  vim.cmd.colorscheme(color)
+end
+
+function LineNumberColors()
+  vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'gray', bold = true })
+  vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = false })
+  vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'gray', bold = true })
 end
 
 ApplyColorScheme()
+LineNumberColors()
