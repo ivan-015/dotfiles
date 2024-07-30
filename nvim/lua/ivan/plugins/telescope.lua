90a9ca69c3a2b3724e3581cb73ab4feb30b38261
Ivan Vigliante <ivan.avr@protonmail.com>
2024-07-29 21:10:40 -0700

    Remove custom search from telescope

--- nvim/lua/ivan/plugins/telescope.lua
+++ nvim/lua/ivan/plugins/telescope.lua
@@ -3,13 +3,7 @@ return {
   tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('telescope').setup({
-      pickers = {
-        find_files = {
-          find_command = { 'rg', '--files', '--hidden', '--glob', '!.git', '--glob', '*.env' }
-        }
-      }
-    })
+    require('telescope').setup()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
